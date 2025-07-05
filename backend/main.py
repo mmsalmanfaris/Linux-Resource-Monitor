638d1cf6138439570e60068ceb928aaeb1f0d97a
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2025-07-06 00:35:22 +0530

    psutil setup

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import psutil
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return{"message":" Hello Monitoring System"}
+
+@app.get("/usage")
+async def usage():
+    return {
+        "cpu_percent": psutil.cpu_percent(interval=None),
+        "cpu_count": psutil.cpu_count(),
+        "cpu_freq": psutil.cpu_freq(),
+        "v_memory": psutil.virtual_memory(),
+        "swap_memory": psutil.swap_memory(),
+        "memory_usage": "30%",
+        "disk_usage": "40%"
+    }
