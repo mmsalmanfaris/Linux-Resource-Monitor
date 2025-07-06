5a3491e1dc913e9bdff5004452b1bdd2c8ad7385
MM Salman Faris <110341513+mmsalmanfaris@users.noreply.github.com>
2025-07-06 22:06:18 +0530

    Automation script

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -xeo pipefail
+
+#####################################
+# Script to run the Linux Resource Monitor
+# Author: Salman Faris
+# Version: v1.0
+# Description: This script clones the Linux Resource Monitor repository and runs both backend and frontend.
+#####################################
+
+# Clone the repository
+git clone https://github.com/mmsalmanfaris/Linux-Resource-Monitor.git
+cd Linux-Resource-Monitor
+
+# ----------------------
+# Run Backend
+# ----------------------
+cd backend
+
+pip install -r requirements.txt
+
+uvicorn main:app --host 0.0.0.0 --port 8000 &
+
+# ----------------------
+# Run Frontend
+# ----------------------
+cd ../frontend
+
+npm install
+
+npm run dev &
+
+sleep 2
+
+xdg-open http://localhost:5173
